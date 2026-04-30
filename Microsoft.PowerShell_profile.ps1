3689c58740de959f4848aa5f9edddd01e6845b39
fuciktomas <fucik.tomas@gmail.com>
2026-04-30 09:32:30 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,3 +11,8 @@ Import-Module -Name PSReadLine
 #Prompt
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
 oh-my-posh init pwsh --config "catppuccin" | Invoke-Expression
+
+#Copilot
+# https://docs.github.com/en/copilot/how-tos/copilot-cli/customize-copilot/use-byok-models
+#$env:COPILOT_PROVIDER_BASE_URL = "http://localhost:11434/v1"
+#$env:COPILOT:MODEL = "llama3.2:latest"
