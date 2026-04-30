38b7c92653657b3c569b95f353f496666511a703
fuciktomas <fucik.tomas@gmail.com>
2026-04-30 09:26:52 +0200

    Update oh-my-posh theme configuration

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,10 @@ Set-Alias ll ls
 Set-Alias g git
 Set-Alias vim nvim
 
+#Modules
 Import-Module -Name Terminal-Icons
-
 Import-Module -Name PSReadLine
 
 #Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "catppuccin" | Invoke-Expression
